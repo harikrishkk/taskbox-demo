49fdf44ab9a506563b932d0e4b7924120783d469
Hari <harikrishkk@gmail.com>
2023-02-20 22:47:55 -0500

    more updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './index.css';
 import store from './lib/store';
-
 import { Provider } from 'react-redux';
 import InboxScreen from './components/InboxScreen';
+import { Button } from '@harikrishkk/component-library';
 
 function App() {
   return (
     <Provider store={store}>
       <InboxScreen />
+      <Button variant="primary" size="medium">
+        Log in / Register
+      </Button>
     </Provider>
   );
 }
